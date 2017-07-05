26478238acecfbe46be599b10ba46e7d66f7b435
plammel <lammelpablo@gmail.com>
2017-07-05 16:43:26 -0400

    Update main.js

--- main.js
+++ main.js
@@ -1,42 +1,72 @@
-const
-    request = require('request'),
-    cheerio = require('cheerio'),
-	csv=require('csvtojson')
-	inputFile='list.csv'
-;
+function run () {
 	
-function readScripts(url) {
-    request(url, function (error, response, body) {
-        var $ = cheerio.load(body);
-		var srcs = [];
-		
-		$('script').each(function () {
-            var src = $(this).attr('src');
-			if(src) {
-				srcs[srcs.length] = src;
-			}
-        });
-		
-		console.log("src " + srcs.length + ' / ' + $('script').length);
-		console.log(srcs);
-    })
-}
-
-var run = function () {
+		const
+		request = require('request'),
+		cheerio = require('cheerio'),
+		csv=require('csvtojson')
+		csvFilePath='list.csv'
+	;
+	
 	var webs = [];
-	const csvFilePath='list.csv';
+	
+	function Web(name, url, sources) {
+		this.name = name; 
+		this.url = url;
+		this.sources = sources;
+	} 
+
+	var readScripts = function (url) {
+		request(url, function (error, response, body) {
+			var $ = cheerio.load(body);
+			var srcs = [];
+			
+			$('script').each(function () {
+				var src = $(this).attr('src');
+				if(src) {
+					srcs[srcs.length] = src;
+				}
+			});		
+		})
+	}
+
+	var loadScriptSources = function(websPrint, webs, end, callback) {
+		console.log('loadScriptSources ' + webs.length)
+		request(webs[0].url, function (error, response, body) {
+			var $ = cheerio.load(body);
+			var srcs = [];
+			
+			$('script').each(function () {
+				var src = $(this).attr('src');
+				if(src) {
+					srcs[srcs.length] = src;
+				}
+			});
+
+			webs[0].sources = srcs;
+			console.log('loadScriptSources ' + webs.length + ' > sources loaded');
+			
+			if (webs.length > 1) {
+				return callback(websPrint, webs.slice(1, webs.length+1), end, loadScriptSources);
+			} else {
+				return printWebs(websPrint);
+			}
+		})
+	}	
+
+	var printWebs = function (webs) {
+		console.log('\n' + ' -------- PRINT WEBS -------- ' + '\n');
+		console.log(webs);
+		
+	}
 
 	csv()
 	.fromFile(csvFilePath)
 	.on('json',(jsonObj)=>{
-		webs[webs.length] = jsonObj; 
+		webs[webs.length] = new Web(jsonObj.name, jsonObj.url); 
 	})
-	.on('done',(error)=>{		
-		for (i in webs) {
-			readScripts(webs[i]);
-		}
+	.on('done',(error)=>{
+		return loadScriptSources(webs, webs, webs.length, loadScriptSources);
 	})
-	
 }
 
 run();
